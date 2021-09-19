ffa417c993f4e9e5af91ba8d8c3228700bec63b6
Geoff Crg <g.craighero@gmail.com>
2021-09-19 17:24:26 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const helmet = require('helmet'); // Protection certaines vulnérabilités connues du web en configurant correctement les headers
+const dotenv = require('dotenv').config();/* Initialisation de dotenv (permet de masquer les données sur notre environnement de développement
+                                            (ici adresse et mot de passe de notre base de donnée.)*/
+
+const cors = require('cors'); /* contourner les erreur de CORS (Système de sécurité CORS : Cross Origin Resource Sharing  
+  La sécurité CORS est une mesure de sécurité par défaut pour empêcher 
+  l'utilisation de ressources par des origines non-autorisées.*/
+
+const express = require('express'); // Framework pour Node.js
+// eslint-disable-next-line import/no-extraneous-dependencies
+//const bodyParser = require('body-parser'); // Parser JSON pour rendre exploitables les objets retournés
+
+const path = require('path'); // Nécessaire pour Multer (importation d'image )
+
+
+
+
+
+const app = express();
+
+// Création de la base et des tables si inexistantes
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use('/images',express.static(path.join(__dirname, '/images')))
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.dbHost,
+  user: process.env.dbUser,
+  password: process.env.dbPassword,
+});
+
+
+
+
+module.exports = app;
