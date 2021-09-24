f17042dd0eec7624c98851ffc7855d28d13a4e85
Geoff Crg <g.craighero@gmail.com>
2021-09-24 13:33:20 +0200

    conditions vue

--- frontend/src/views/UserPostView.vue
+++ frontend/src/views/UserPostView.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="headerInscr">
     <h2 class="title2">Fil d'actualité</h2>
-   <create-post-form />
+    <create-post-form v-if="connectedUserId "/>
 
     <p class="inscrP">
       Ici, vous pouvez lire ce que vos collègues ont écrit ou partagé...
     </p>
-    <Post />
+    <Post  />
   </div>
 </template>
 
 <script>
-
-import CreatePostForm from "../components/CreatePostForm.vue"
+import CreatePostForm from "../components/CreatePostForm.vue";
 import Post from "../components/Post";
 export default {
   name: "AllPost",
   components: {
     Post,
     CreatePostForm,
+  },
+    data() {
+    return {
+      connectedUserId: localStorage.getItem("Id"),
+      first_name: "",
+      last_name: "",
+      posts: [],
+      title: "",
+      body: "",
+      idUser: "",
+    };
   },
 };
 </script>
@@ -30,11 +40,10 @@ export default {
   align-items: center;
   background-color: #fff;
 }
-.title2{
-    text-align: center;
-    color: #1e81b0;
-    font-size: 30px;
-    
+.title2 {
+  text-align: center;
+  color: #1e81b0;
+  font-size: 30px;
 }
 .inscrP {
   text-align: center;
